11799d352d26a6a95a01947f6dc67d8d5d60d105
prachiporwal768 <25116cn463@glbitm.ac.in>
2026-04-13 12:49:05 +0530

    degree to fahrenhite

---
+++ degree to fahrenhiet.cpp
@@ -0,0 +1,11 @@
+#include<stdio.h>
+#include<math.h>
+int main()
+{float c,h;
+printf("enter the temp in centigrade:\n");
+scanf("%f",&c);
+h=c*9.0/5.0+32;
+printf("temp in fahrenheit:%f\n");
+return 0;
+
+}
