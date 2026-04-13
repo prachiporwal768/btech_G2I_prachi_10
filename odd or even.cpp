83d028c9d09abc730a531f3e79a19191015faf6e
prachiporwal768 <25116cn463@glbitm.ac.in>
2026-04-13 12:54:13 +0530

    odd or even

---
+++ odd or even.cpp
@@ -0,0 +1,11 @@
+#include<stdio.h>
+int main()
+{int num;
+printf("enter the value of number\n");
+scanf("%d",&num);
+if(num%2==0)
+{printf("the number is even \n");
+}
+else{printf("the number is odd\n");
+} return 0;
+}
