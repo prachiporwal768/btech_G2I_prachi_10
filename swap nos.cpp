4e03f3a6aa08c9a608f4e9c38e1f1fbabaeae116
prachiporwal768 <25116cn463@glbitm.ac.in>
2026-04-13 12:54:54 +0530

    swap no.

---
+++ swap nos.cpp
@@ -0,0 +1,13 @@
+#include<stdio.h>
+#include<math.h>
+int main()
+{int a,b,c;
+printf("enter the value of a,b");
+scanf("%d %d",&a,&b);
+c=a;
+a=b;
+b=c;
+printf("swap numbers are=%d\n");
+return 0;
+
+}
